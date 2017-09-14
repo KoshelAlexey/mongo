23f776d96bd9c2f012121cebdd5a87011febe10c
KoshelAlexey <alexey.koshel87@gmail.com>
2017-09-14 14:12:57 +0300

    recursion fix

--- public/javascripts/relationsConception.js
+++ public/javascripts/relationsConception.js
@@ -1,18 +1,19 @@
 var blackList = [];
 var stopRecur = [];
+var reservedName = [];
 var finalResult =[];
 var allCol = {};
-var allrel = {};
+var allRel = {};
 
 function relationsConception(data) {
     var collections = data.collections;
     allCol=collections;
     var relations = data.relations;
-    allrel = relations;
+    allRel = relations;
     var res = [];
 
     collectionBuild(collections,relations);
-    res = finalResult
+    res = finalResult;
     finalResult = [];
     blackList = [];
     return res
@@ -25,12 +26,13 @@ function collectionBuild(col,rel,recurs) {
     var rec = recurs;
 
     for(var i = 0; collections[i] !== undefined;i++) {
-        if(!existCheck(collections[i].name)){
+        if(!existCheck(collections[i].name) && !reserveCheck(collections[i].name)){
+            reservedName.push(collections[i].name);
             if(!blackListCheck(collections[i].name) || rec === 1){
                 var instance = {};
                 for(var j = 0; collections[i].fields[j]!== undefined;j++){
                     if(collections[i].fields[j].type === "rel"){
-                        instance[collections[i].fields[j].name] = relControl(collections[i].fields[j],allCol,allrel,collections[i].name)
+                        instance[collections[i].fields[j].name] = relControl(collections[i].fields[j],allCol,allRel,collections[i].name)
                     }
                     else{
                         instance[collections[i].fields[j].name] = collections[i].fields[j];
@@ -44,6 +46,7 @@ function collectionBuild(col,rel,recurs) {
         else{
             continue
         }
+        reservedName = [];
         stopRecur = [];
         // instance = objEqals(instance);
         instance.collectionName = collections[i].name;
@@ -54,6 +57,155 @@ function collectionBuild(col,rel,recurs) {
     //     finalResult=res
     // }
 
+}
+
+
+
+function relControl(thisField,col,rel,colName) {
+    var thisColName = colName;
+    var field = thisField;
+    var allCollect = col;
+    var allRelat = rel;
+
+    stopRecur.push(thisColName);
+    var res = {};
+    var thisRel = (function () {
+        for(var r = 0; allRelat[r] !== undefined;r++) {
+            if (field.name === allRelat[r].beginField
+                && thisColName === allRelat[r].beginCollection) {
+                return allRelat[r]
+            }
+        }
+    })();
+    if(!thisRel){
+        return "relation don't used"
+    }
+    var endCol = (function () {
+        for(var c = 0; allCollect[c] !== undefined;c++) {
+            if(allCollect[c].name === thisRel.endCollection){
+                return allCollect[c]
+            }
+        }
+    })();
+
+
+    switch (thisRel.type) {
+        case 'oneToOne': {
+            if (!relationsCheck(endCol.name)){
+                blackList.push(endCol.name);
+            };
+            for(var f = 0; endCol.fields[f] !== undefined;f++) {
+                if(endCol.fields[f].type === "rel") {
+                    if (recursionCheck(endCol.name)) {
+                        res[endCol.fields[f].name] = "recursion"
+                    }
+                    else {
+                        res[endCol.fields[f].name] = relControl(endCol.fields[f], allCollect, allRelat, endCol.name, colName);
+                    }
+
+                }
+                else{
+                    res[endCol.fields[f].name] = endCol.fields[f];
+                }
+            }
+
+            return res
+        }
+            break;
+        case 'oneToMany': {
+            res.emb = {};
+            res.link = {};
+            if(field.maxValue < 50){
+                for(var f = 0; endCol.fields[f] !== undefined;f++) {
+                    if(endCol.fields[f].type === "rel") {
+                        if (!recursionCheck(endCol.name)) {
+                            res.emb[endCol.fields[f].name] = relControl(endCol.fields[f], allCollect, allRelat, endCol.name);
+                        }
+                        else{
+                            res.emb[endCol.fields[f].name] = "recursion"
+                        }
+                    }
+                    else{
+                        res.emb[endCol.fields[f].name] = endCol.fields[f];
+                    }
+                }
+            }
+            else{
+                res.emb = "Too many documents. Recomed value <50"
+            }
+
+            var rec = 1;
+            var colArr = [];
+            colArr.push(endCol);
+            collectionBuild(colArr,allRelat,rec);
+            res.link.id = "link to: "+ endCol.name;
+
+            return res
+        }
+            break;
+        case 'manyToOne': {
+            res.emb = {};
+            res.link = {};
+            for(var f = 0; endCol.fields[f] !== undefined;f++) {
+                if(endCol.fields[f].type === "rel") {
+                    if (!recursionCheck(endCol.name)) {
+                        res.emb[endCol.fields[f].name] = relControl(endCol.fields[f], allCollect, allRelat, endCol.name);
+                    }
+                    else{
+                        res.emb[endCol.fields[f].name] = "recursion"
+                    }
+                }
+                else{
+                    res.emb[endCol.fields[f].name] = endCol.fields[f];
+                }
+            }
+
+            var rec = 1;
+            var colArr = [];
+            colArr.push(endCol);
+            collectionBuild(colArr,allRelat,rec);
+            res.link.id = "link to: "+ endCol.name;
+
+            return res
+        }
+            break;
+        case 'manyToMany': {
+            var rec = 1;
+            var colArr = [];
+            colArr.push(endCol);
+            collectionBuild(colArr,allRelat,rec);
+            res.id = "link to: "+ endCol.name;
+            return res
+        }
+            break;
+    }
+}
+
+// function objEqals(obj) {
+//     var firstObj = obj.embed;
+//     var secondObj = obj.link;
+//     var s1 = JSON.stringify(firstObj);
+//     var s2 = JSON.stringify(secondObj);
+//     if(s1 === s2){
+//         return firstObj
+//     }
+//     else{
+//         return obj
+//     }
+// }
+function reserveCheck(collectionName) {
+    var colName = collectionName;
+    if(reservedName.some(
+            (item)=>{
+                if(item===colName){
+                    return true
+                }
+            })){
+        return true
+    }
+    else{
+        return false
+    }
 }
 
 function recursionCheck(collectionName) {
@@ -89,11 +241,11 @@ function existCheck(collectionName) {
 function blackListCheck(collectionsName){
     var colName = collectionsName;
     if(blackList.some(
-        (item)=>{
-            if(item===colName){
-                return true
-            }
-        })){
+            (item)=>{
+                if(item===colName){
+                    return true
+                }
+            })){
         return true
     }
     else{
@@ -101,130 +253,29 @@ function blackListCheck(collectionsName){
     }
 }
 
-function relControl(thisField,col,rel,colName) {
-    var thisColName = colName;
-    var field = thisField;
-    var allCol = col;
-    var allRel = rel;
+function relationsCheck(collectionsName) {
+    var thisColName = collectionsName;
+    var allCollect = allCol;
+    var allRelat = allRel
 
-    stopRecur.push(thisColName);
-    var res = {};
-    var thisRel = (function () {
-        for(var r = 0; allRel[r] !== undefined;r++) {
-            if (field.name === allRel[r].beginField
-                && thisColName === allRel[r].beginCollection) {
-                return allRel[r]
-            }
-        }
-    })();
-    if(!thisRel){
-        return "relation don't used"
-    }
-    var endCol = (function () {
-        for(var c = 0; allCol[c] !== undefined;c++) {
-            if(allCol[c].name === thisRel.endCollection){
-                return allCol[c]
-            }
-        }
-    })();
-
-
-    switch (thisRel.type) {
-        case 'oneToOne': {
-            blackList.push(endCol.name);
-            for(var f = 0; endCol.fields[f] !== undefined;f++) {
-                if(endCol.fields[f].type === "rel") {
-                    if (recursionCheck(endCol.name)) {
-                        res[endCol.fields[f].name] = "recursion"
-                    }
-                    else {
-                        res[endCol.fields[f].name] = relControl(endCol.fields[f], allCol, allRel, endCol.name, colName);
+    for(var i = 0; allCollect[i] !== undefined;i++) {
+        if (allCollect[i].name === thisColName) {
+            for (var j = 0; allCollect[i].fields[j] !== undefined; j++) {
+                if (allCollect[i].fields[j].type === "rel") {
+                    for (var r = 0; allRelat[r] !== undefined; r++) {
+                        if (allCollect[i].fields[j].name === allRelat[r].beginField
+                            && thisColName === allRelat[r].beginCollection) {
+                            if (allRelat[r].type !== "oneToOne") {
+                                return true
+                            }
+                        }
                     }
 
-                }
-                else{
-                    res[endCol.fields[f].name] = endCol.fields[f];
                 }
             }
-
-            return res
+            return false
         }
-            break;
-        case 'oneToMany': {
-            res.emb = {};
-            res.link = {};
-            for(var f = 0; endCol.fields[f] !== undefined;f++) {
-                if(endCol.fields[f].type === "rel") {
-                    if (!recursionCheck(endCol.name)) {
-                        res.emb[endCol.fields[f].name] = relControl(endCol.fields[f], allCol, allRel, endCol.name);
-                    }
-                    else{
-                        res.emb[endCol.fields[f].name] = "recursion"
-                    }
-                }
-                else{
-                    res.emb[endCol.fields[f].name] = endCol.fields[f];
-                }
-            }
-
-            // var rec = 1;
-            // var colArr = [];
-            // colArr.push(endCol);
-            // collectionBuild(colArr,allRel,rec);
-            res.link.id = "link to: "+ endCol.name;
-
-            return res
-        }
-            break;
-        case 'manyToOne': {
-            res.emb = {};
-            res.link = {};
-            for(var f = 0; endCol.fields[f] !== undefined;f++) {
-                if(endCol.fields[f].type === "rel") {
-                    if (!recursionCheck(endCol.name)) {
-                        res.emb[endCol.fields[f].name] = relControl(endCol.fields[f], allCol, allRel, endCol.name);
-                    }
-                    else{
-                        res.emb[endCol.fields[f].name] = "recursion"
-                    }
-                }
-                else{
-                    res.emb[endCol.fields[f].name] = endCol.fields[f];
-                }
-            }
-
-            // var rec = 1;
-            // var colArr = [];
-            // colArr.push(endCol);
-            // collectionBuild(colArr,allRel,rec);
-            res.link.id = "link to: "+ endCol.name;
-
-            return res
-        }
-            break;
-        case 'manyToMany': {
-            var rec = 1;
-            var colArr = [];
-            colArr.push(endCol);
-            collectionBuild(colArr,allRel,rec);
-            res.id = "link to: "+ endCol.name;
-            return res
-        }
-            break;
     }
 }
-
-// function objEqals(obj) {
-//     var firstObj = obj.embed;
-//     var secondObj = obj.link;
-//     var s1 = JSON.stringify(firstObj);
-//     var s2 = JSON.stringify(secondObj);
-//     if(s1 === s2){
-//         return firstObj
-//     }
-//     else{
-//         return obj
-//     }
-// }
 
 module.exports = relationsConception;
